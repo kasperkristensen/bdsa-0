c184dc6a87de4f4610c64c3b1a5990a4a5d8277d
Kasper <kasper@medusa-commerce.com>
2021-09-08 17:19:02 +0200

    added latex docs

--- csharp_boilerplate/Calender/Calender.cs
+++ csharp_boilerplate/Calender/Calender.cs
@@ -6,7 +6,7 @@ namespace csharp_boilerplate {
             if (year < 1582) {
                 throw new ArgumentException(String.Format("{0} is not a valid input, as it must be greater than or equal to 1582", year),"year");
             }
-
+                
             return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
         }
     }
