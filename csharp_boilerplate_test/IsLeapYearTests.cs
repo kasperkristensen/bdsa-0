f12d08f68d2f0031405ca4a43df8a2aa449dae0c
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2021-09-08 17:01:33 +0200

    feat: added isLeapYear and tested it (#1)
    * added isLeapYear and tested it
    
    * added user input to main
    
    * added error handling

---
+++ csharp_boilerplate_test/IsLeapYearTests.cs
@@ -0,0 +1,47 @@
+using System;
+using Xunit;
+using csharp_boilerplate;
+
+namespace csharp_boilerplate_test
+{
+    public class isLeapYearTests
+    {
+        [Fact]
+        public void if_divisible_by_4_return_true()
+        {
+            bool actual = Calender.isLeapYear(2024);
+
+            Assert.Equal(true, actual);
+        }
+
+        [Fact]
+        public void if_not_divisible_by_4_return_false()
+        {
+            bool actual = Calender.isLeapYear(2019);
+
+            Assert.Equal(false, actual);
+        }
+
+        [Fact]
+        public void if_divisible_by_100_return_false()
+        {
+            bool actual = Calender.isLeapYear(1800);
+
+            Assert.Equal(false, actual);
+        }
+
+        [Fact]
+        public void if_divisible_by_100_and_400_return_true()
+        {
+            bool actual = Calender.isLeapYear(2000);
+
+            Assert.Equal(true, actual);
+        }
+
+        [Fact]
+        public void if_year_is_less_than_1582_throw()
+        {
+            Assert.Throws<ArgumentException>(() => Calender.isLeapYear(1500));
+        }
+    }
+}
